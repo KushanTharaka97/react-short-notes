66d7a5d86d718a1059243b1b29c135b980a2dfc7
KushanTharakaPeiris <kushantharakapeiris@gmail.com>
2022-02-28 14:55:39 +0530

    INITIAL COMMIT

--- src/index.js
+++ src/index.js
@@ -1,17 +1,136 @@
-import React from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function CheckBox(){
+  const [check, setCheck] = useState(false);
+
+  useEffect(() => {
+    alert(`checked:  ${check.toString()}`);
+  });
+  return(
+    <>
+      <input type="checkbox" value={check} onChange={() => setCheck(check => !check)} />
+      {check  ? "CHECKED" : "NOT CHECKED"}
+    </>
+  )
+}
+
+function App(props){
+ const [state, setState] = useState(0);
+ const [manager, setManager] =useState("Manager1");
+ const [year, setYear] = useState(2020)
+ return (<div>
+   <h1>Manger : {manager}</h1>
+   <button onClick={()=> setManager("Manger2")}>
+     Change Manager
+   </button>
+
+   <h2>YEAR : {year} </h2>
+   <button onClick={() => setYear(year+1)}>
+      YEAR
+  </button>
+
+
+   <h1>Status: {state}</h1>
+
+  <button onClick={() => setState("Closed")}>
+      CLOSED
+  </button>
+
+  <button onClick={() => setState("Breask in 5 min")}>
+      BREAK
+  </button>
+
+  <button onClick={() => setState("open")}>
+      OPEN
+  </button>
+
+ </div>);
+}
+
+
+function APP2(){
+  const[val, setVal] = useState("");
+  const[val2, setVal2] = useState("");
+
+  useEffect(() => {
+    console.log(`field 1 : ${val}`)
+  }, [val]);
+
+  useEffect(() => {
+    console.log(`field 2 : ${val2}`)
+  }, [val2]);
+
+  return(
+    <>
+      <label> 
+        Favorite Places: 
+      </label>
+      <input 
+      value={val}
+      onChange={e => setVal(e.target.value)} />
+
+      <label>
+      Second Favorite Places :
+      </label>
+      <input 
+      value={val2}
+      onChange={e => setVal2(e.target.value)} />
+    </>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+}
+
+function GitHubUser({login}){
+  const[data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+    .then(res => res.json())
+    .then(setData)
+    .catch(console.error);
+  }, [])
+
+
+  if(data){
+    return (
+    <div>
+      <h1>{data.login}</h1>
+      <img src={data.avatar_url} width="10%"></img>
+    </div>
+    );
+  }
+  return null;
+}
+
+function APPgit(){
+  return <GitHubUser login="eveporcello" />;
+}
+
+function CheckBoxWithReducer(){
+  const[checked, toggle] = useReducer(
+    checked => !checked,
+    false
+    );
+
+  return(
+      <>
+        <input type="checkbox" value={checked} onChange={toggle}/>
+        {checked ? "Checked": "NOT CHECKED"}
+      </>
+  );
+}
+
+ReactDOM.render(
+  // <Hello library="lib_value1" number={10} />,
+  // <PractisingHello propsParsed="Value of props"/>,
+  // <App seasons={0}/>,
+  // <CheckBox/>,
+  // <APP2 />,
+  // <APPgit />,
+  <CheckBoxWithReducer />,
+  document.getElementById('root')
+);
